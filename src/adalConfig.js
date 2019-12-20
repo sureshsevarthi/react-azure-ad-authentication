495959ea7328471bf0aeca90330c220a111f7611
Suresh Sevarthi <43616597+sureshsevarthi@users.noreply.github.com>
2019-12-20 14:32:57 -0500

    Update adalConfig.js

--- src/adalConfig.js
+++ src/adalConfig.js
@@ -2,9 +2,9 @@ import { AuthenticationContext, adalFetch, withAdalLogin } from 'react-adal';
 
 export const adalConfig = {
   tenant: 'tenant.onmicrosoft.com',
-  clientId: '2ffb8050-3150-4d20-9f1e-2e7bf2d42',
+  clientId: 'clientId',
   endpoints: {
-    api: 'e93198fd-f287-4aad-99d1-4b543d420791',
+    api: 'clientId',
   },
   cacheLocation: 'localStorage',
   postLogoutRedirectUri: 'http://localhost:3000/home',
